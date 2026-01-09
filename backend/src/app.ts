ada88b96ccefaf3b0d5b5f68543f84ce2ba53b5e
shubhpatel2197 <202151111@iiitvadodara.ac.in>
2026-01-09 17:11:20 +0530

    testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth'
 import userRoutes from './routes/user' 
 import { requireAuth } from './middleware/auth'
 import groupRoutes from './routes/groups'
+import  opentelemetry  from '@opentelemetry/api'
 
 const app = express()
 
@@ -23,8 +24,27 @@ app.use(
 app.use(express.json())
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  const sessionID = "shubh";
+  console.log("sessionID", sessionID);
+  if (sessionID) {
+    // 1. Get the current active Span (the one OTel just started)
+    const currentSpan = opentelemetry.trace.getSpan(opentelemetry.context.active());
+
+    if (currentSpan) {
+      // 2. Add your custom tag (This is what makes it searchable!)
+      currentSpan.setAttribute('bug.session_id', sessionID);
+      
+      // Optional: Add more context if useful
+      currentSpan.setAttribute('bug.reporter_ip', req.ip);
+    }
+  }
+  next();
+});
+
 // public routes
 app.use('/auth', authRoutes)
+
 
 // protected routes
 app.use('/user', requireAuth(), userRoutes)
