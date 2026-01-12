4fd84e30f092658e10081228c65017a67da34d50
shubhpatel2197 <202151111@iiitvadodara.ac.in>
2026-01-12 16:39:49 +0530

    bug fix

--- backend/src/telementry/context.ts
+++
@@ -1,4 +0,0 @@
-import { createContextKey } from '@opentelemetry/api';
-
-// This is the unique symbol used to store the Bug Session ID inside the OTel Context
-export const BUG_SESSION_KEY = createContextKey('BugSessionID');
