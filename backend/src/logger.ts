22603fa95a1c9bc509ae2b7647992184a6e9d064
shubhpatel2197 <202151111@iiitvadodara.ac.in>
2026-02-03 14:10:58 +0530

    bugfix

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -1,38 +1,38 @@
-import pino from 'pino'
-import { trace, context } from '@opentelemetry/api'
-import fs from 'fs'
-import path from 'path'
+// import pino from 'pino'
+// import { trace, context } from '@opentelemetry/api'
+// import fs from 'fs'
+// import path from 'path'
 
-const logDir = path.join(process.cwd(), 'logs')
+// const logDir = path.join(process.cwd(), 'logs')
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true })
-}
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir, { recursive: true })
+// }
 
-const logFile = path.join(logDir, 'app.log')
+// const logFile = path.join(logDir, 'app.log')
 
-export const logger = pino(
-  {
-    level: 'info',
-    base: {
-      service: 'project-1-backend',
-    },
-    mixin() {
-      const span = trace.getSpan(context.active())
+// export const logger = pino(
+//   {
+//     level: 'info',
+//     base: {
+//       service: 'project-1-backend',
+//     },
+//     mixin() {
+//       const span = trace.getSpan(context.active())
 
-      if (!span) return {}
+//       if (!span) return {}
 
-      const { traceId, spanId } = span.spanContext()
+//       const { traceId, spanId } = span.spanContext()
 
-      return {
-        trace_id: traceId,
-        span_id: spanId,
-      }
-    },
-    timestamp: pino.stdTimeFunctions.isoTime,
-  },
-  pino.destination({
-    dest: logFile,
-    sync: false, // async write (recommended)
-  })
-)
+//       return {
+//         trace_id: traceId,
+//         span_id: spanId,
+//       }
+//     },
+//     timestamp: pino.stdTimeFunctions.isoTime,
+//   },
+//   pino.destination({
+//     dest: logFile,
+//     sync: false, // async write (recommended)
+//   })
+// )
